1edee76709b70c12d7941666fa065fec31fad2ef
MathisBullinger <mathis@bullinger.dev>
2020-02-09 16:31:41 +0100

    feat: setup knex

---
+++ src/db.ts
@@ -0,0 +1,11 @@
+import knex from 'knex'
+
+export default knex({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+})
