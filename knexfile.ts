a1159dab0f9a0fb48ea5929e19df6be3658a7626
MathisBullinger <mathis@bullinger.dev>
2020-02-27 22:09:08 +0100

    feat: setup knex migrations

---
+++ knexfile.ts
@@ -0,0 +1,20 @@
+require('dotenv').config()
+require('ts-node/register')
+
+module.exports = {
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  },
+  migrations: {
+    directory: './db/migrations',
+    extension: 'ts',
+  },
+  seeds: {
+    directory: './db/seeds/dev',
+  },
+}
