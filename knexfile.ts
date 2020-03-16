737957c26aa09122815ac003c90caa729f29500f
Mathis Bullinger <mathis@bullinger.dev>
2020-03-16 16:16:42 +0100

    feat: setup production db

--- knexfile.ts
+++ knexfile.ts
@@ -2,19 +2,40 @@ require('dotenv').config()
 require('ts-node').register({ compilerOptions: { module: 'commonjs' } })
 
 module.exports = {
-  client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME_DEV,
+    },
+    migrations: {
+      directory: './db/migrations',
+      extension: 'ts',
+    },
+    seeds: {
+      directory: './db/seeds/dev',
+      extension: 'ts',
+    },
   },
-  migrations: {
-    directory: './db/migrations',
-    extension: 'ts',
-  },
-  seeds: {
-    directory: './db/seeds/dev',
+  production: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME_PROD,
+    },
+    migrations: {
+      directory: './db/migrations',
+      extension: 'ts',
+    },
+    seeds: {
+      directory: './db/seeds/prod',
+      extension: 'ts',
+    },
   },
 }
--- src/db.ts
+++ src/db.ts
@@ -14,7 +14,12 @@ export default knex({
     ...(!isNaN(port) && { port }),
     user: process.env.DB_USER ?? get('USER'),
     password: process.env.DB_PASS ?? get('PASS'),
-    database: process.env.DB_NAME ?? get('NAME') ?? 'api',
+    database:
+      (process.env.NODE_ENV !== 'production'
+        ? process.env.DB_NAME_DEV
+        : process.env.DEB_NAME_PROD) ??
+      get('NAME') ??
+      'api',
   },
   pool: { min: 0, max: 20 },
   debug: !!process.env.IS_OFFLINE,
