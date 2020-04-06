0d46283d478fa73b10fda35ee7027ab7152995ae
Mathis Bullinger <mathis@bullinger.dev>
2020-04-06 04:34:39 +0200

    chore: update knexfile

--- knexfile.ts
+++ knexfile.ts
@@ -2,40 +2,20 @@ require('dotenv').config()
 require('ts-node').register({ compilerOptions: { module: 'commonjs' } })
 
 module.exports = {
-  development: {
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME_DEV,
-    },
-    migrations: {
-      directory: './db/migrations',
-      extension: 'ts',
-    },
-    seeds: {
-      directory: './db/seeds/dev',
-      extension: 'ts',
-    },
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   },
-  production: {
-    client: 'pg',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
-    },
-    migrations: {
-      directory: './db/migrations',
-      extension: 'ts',
-    },
-    seeds: {
-      directory: './db/seeds/prod',
-      extension: 'ts',
-    },
+  migrations: {
+    directory: './db/migrations',
+    extension: 'ts',
+  },
+  seeds: {
+    directory: './db/seeds/prod',
+    extension: 'ts',
   },
 }
