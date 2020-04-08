10b7513371064e0117f7e989ffef4bc96687f798
Mathis Bullinger <mathis@bullinger.dev>
2020-04-08 18:45:29 +0200

    feat: handle accents & umlauts in search

--- src/resolvers/queries.ts
+++ src/resolvers/queries.ts
@@ -4,6 +4,9 @@ import { generateAuthUrl } from '../gcal'
 import knex from '../db'
 import resolver from './resolver'
 import { system } from '../authorization/user'
+import levenshtein from 'fast-levenshtein'
+import _ from 'lodash'
+import { filterKeys } from '../utils/object'
 
 export const me = resolver<User>().loggedIn(
   async ({ query, ctx: { id } }) => await query().findById(id)
@@ -39,7 +42,7 @@ export const user = resolver<User>()(
     const user = await query()
       .where(id ? { 'users.id': id } : { handle })
       .first()
-    if (!mentor) throw handleError(`can't find user ${handle ?? id}`)
+    if (!user) throw handleError(`can't find user ${handle ?? id}`)
     return user
   }
 )
@@ -76,3 +79,77 @@ export const isTokenValid = resolver<boolean>()(
     return id && id !== token.subject ? false : !!token
   }
 )
+
+export const search = resolver<any>()(async ({ args: { term, maxUsers } }) => {
+  term = term.toLowerCase()
+
+  let usersRaw = await knex('users')
+    .select(
+      'users.id',
+      'name',
+      'handle',
+      'role',
+      'score',
+      'url',
+      'size',
+      'type'
+    )
+    .leftJoin('mentors', { 'users.id': 'mentors.id' })
+    .leftJoin('profile_pictures', { 'users.id': 'profile_pictures.user_id' })
+    .whereNot('role', 'nologin')
+    .where(
+      'name_normalized',
+      'ilike',
+      knex.raw(`('%' || unaccent('${term}') || '%')`)
+    )
+
+  usersRaw = _(usersRaw)
+    .groupBy('id')
+    .values()
+    .value()
+    .flatMap(list =>
+      list.slice(1).reduce(
+        (a, c) => ({
+          ...a,
+          profile_pictures: [
+            ...a.profile_pictures,
+            filterKeys(c, ['url', 'size', 'type']),
+          ],
+        }),
+        {
+          ...filterKeys(list[0], k => !['url', 'size', 'type'].includes(k)),
+          profile_pictures: [filterKeys(list[0], ['url', 'size', 'type'])],
+        }
+      )
+    )
+    .map(({ profile_pictures, ...user }) => ({
+      ...user,
+      profile_pictures: profile_pictures.filter(({ size }) => size),
+    }))
+
+  let [users, mentors] = _.partition(usersRaw, { role: 'user' }).map(list =>
+    list
+      .map(user => ({
+        ...user,
+        dist: levenshtein.get(term, user.name.toLowerCase()),
+      }))
+      .sort((a, b) => a.dist - b.dist)
+  )
+
+  mentors = _(mentors)
+    .groupBy('dist')
+    .values()
+    .value()
+    .map(set => set.sort((a: any, b: any) => b.score - a.score))
+    .flat()
+
+  users = [mentors, users].flat()
+
+  console.log(users.map(({ id, profile_pictures, ...user }) => user))
+
+  users = users.slice(0, maxUsers)
+
+  return {
+    users,
+  }
+})
