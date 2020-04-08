e0f98436a65294257dffc0e6e7c178f030b70b86
Mathis Bullinger <mathis@bullinger.dev>
2020-04-09 00:08:52 +0200

    feat: mergeTags mutation

--- src/resolvers/mutations/tag.ts
+++ src/resolvers/mutations/tag.ts
@@ -1,13 +1,24 @@
 import resolver from '../resolver'
 import { UserInputError } from '../../error'
 import { Tags } from '../../models'
+import knex from '../../db'
 
 export const setTagName = resolver<Tags>().isAdmin(
-  async ({ query, args: { id, name } }) => {
-    if (!id || !name) throw new UserInputError('must provide tag id & name')
-    return (
-      (await query().patchAndFetchById(id, { name })) ??
-      new UserInputError('unknown tag')
-    )
+  async ({ query, args: { id, name } }) =>
+    (await query().patchAndFetchById(id, { name })) ??
+    new UserInputError('unknown tag')
+)
+
+export const mergeTags = resolver<Tags>().isAdmin(
+  async ({ query, args: { from, into } }) => {
+    await knex.transaction(async trx => {
+      await trx('user_tags')
+        .where({ tag_id: from })
+        .update({ tag_id: into })
+      await trx('tags')
+        .where({ id: from })
+        .delete()
+    })
+    return query().findById(into)
   }
 )
