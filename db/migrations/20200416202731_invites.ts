3f1f304fd79568b3a3a8e7fbd23e424542d422e7
Mathis Bullinger <mathis@bullinger.dev>
2020-04-16 20:48:10 +0200

    feat: invites table migration

---
+++ db/migrations/20200416202731_invites.ts
@@ -0,0 +1,19 @@
+import * as Knex from 'knex'
+
+export async function up(knex: Knex): Promise<any> {
+  await knex.schema.createTable('invites', t => {
+    t.uuid('id').primary()
+    t.uuid('issuer')
+      .references('users.id')
+      .onDelete('SET TO NULL')
+    t.text('email').notNullable()
+    t.specificType('role', 'user_role').notNullable()
+    t.uuid('redeemed')
+      .references('users.id')
+      .onDelete('CASCADE')
+  })
+}
+
+export async function down(knex: Knex): Promise<any> {
+  await knex.schema.dropTable('invites')
+}
