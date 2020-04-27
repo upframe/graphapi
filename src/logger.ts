7c6148219e98d93823c69c0c9a0d2228ae0dd53a
Mathis Bullinger <mathis@bullinger.dev>
2020-04-27 20:19:05 +0200

    feat: setup logger

---
+++ src/logger.ts
@@ -0,0 +1,27 @@
+import { createLogger, format, transports } from 'winston'
+
+export default createLogger({
+  transports: [
+    new transports.Console({
+      format:
+        process.env.NODE_ENV === 'production'
+          ? format.json()
+          : format.combine(
+              format.colorize(),
+              format.timestamp({
+                format: 'HH:mm:ss',
+              }),
+              format.printf(({ timestamp, level, message, extensions }) => {
+                let msg = `${timestamp} ${level}: ${message}`
+                if (!extensions?.exception?.stacktrace) return msg
+                const stack = []
+                for (const path of extensions.exception.stacktrace.slice(1)) {
+                  if (!path.includes(':/src/')) break
+                  stack.push(path.replace(/\/[\w/.]+:\/src\//, ''))
+                }
+                return msg + `\n${stack.join('\n')}`
+              })
+            ),
+    }),
+  ],
+})
