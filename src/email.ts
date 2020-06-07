b8fd96a6976b018a318f623ffc3b1b5311b7740d
Mathis Bullinger <mathis@bullinger.dev>
2020-06-07 21:04:49 +0200

    feat: require account for contacting users

--- src/email.ts
+++ src/email.ts
@@ -1,17 +1,24 @@
 import { sns } from './utils/aws'
+import logger from './logger'
 
 interface SendOptions {
   template: string
   ctx: any
 }
-export const send = async ({ template, ctx }: SendOptions) =>
-  await sns
-    .publish({
-      Message: JSON.stringify({
-        action: 'SEND_EMAIL',
-        template,
-        ...ctx,
-      }),
-      TopicArn: process.env.EMAIL_SNS,
-    })
-    .promise()
+export const send = async ({ template, ctx }: SendOptions) => {
+  try {
+    await sns
+      .publish({
+        Message: JSON.stringify({
+          action: 'SEND_EMAIL',
+          template,
+          ...ctx,
+        }),
+        TopicArn: process.env.EMAIL_SNS,
+      })
+      .promise()
+  } catch (error) {
+    logger.error("couldn't dispatch email send event", { template, ctx, error })
+    throw error
+  }
+}
