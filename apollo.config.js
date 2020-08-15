9aefe54ee7bd04076eb1b5e8c25917a00b976823
Mathis Bullinger <mathis@bullinger.dev>
2020-08-15 18:43:08 +0200

    build: bundle schema

--- apollo.config.js
+++ apollo.config.js
@@ -1,21 +1,7 @@
-const fs = require('fs')
-
-const readGqlFiles = path =>
-  fs.readdirSync(path).flatMap(file => {
-    return fs.lstatSync(path + file).isDirectory()
-      ? readGqlFiles(path + file + '/')
-      : /\.(gql|graphql)$/.test(file)
-      ? (console.log(`read ${path + file}`),
-        fs.readFileSync(path + file, 'utf-8'))
-      : []
-  })
-
-const files = readGqlFiles('src/schema/')
-
-fs.writeFileSync('./_schema.gql', files.join('\n\n'))
+require('./scripts/bundleSchema')
 
 module.exports = {
   service: {
-    localSchemaFile: './_schema.gql',
+    localSchemaFile: './src/schema/gen/_schema.gql',
   },
 }
---
+++ scripts/bundleSchema.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+
+const readGqlFiles = path =>
+  fs.readdirSync(path).flatMap(file => {
+    return fs.lstatSync(path + file).isDirectory()
+      ? readGqlFiles(path + file + '/')
+      : /\.(gql|graphql)$/.test(file)
+      ? (console.log(`read ${path + file}`),
+        fs.readFileSync(path + file, 'utf-8'))
+      : []
+  })
+
+const files = readGqlFiles('src/schema/')
+
+fs.writeFileSync('src/schema/gen/_schema.gql', files.join('\n\n'))
