19aae5913701aa4bb45c77128e39c1bc4a80cffc
Mathis Bullinger <mathis@bullinger.dev>
2020-10-08 21:24:00 +0200

    feat: add members to space

--- src/resolvers/mutations/space.ts
+++ src/resolvers/mutations/space.ts
@@ -2,7 +2,7 @@ import resolver from '../resolver'
 import uuid from 'uuid/v4'
 import type { Space } from '~/models'
 import { UniqueViolationError } from 'objection'
-import { UserInputError } from '~/error'
+import { ForbiddenError, UserInputError } from '~/error'
 
 export const createSpace = resolver<Space>().isAdmin(
   async ({ query, args: { name, handle = name } }) => {
@@ -13,5 +13,56 @@ export const createSpace = resolver<Space>().isAdmin(
         throw new UserInputError(`handle '${handle}' already in use`)
       throw e
     }
+  }
+)
+
+export const addToSpace = resolver<Space>()(
+  async ({
+    query,
+    args: { spaceId, userIds, mentor, owner },
+    ctx: { id, user },
+    knex,
+  }) => {
+    if (
+      !user.groups.includes('admin') &&
+      !(
+        id &&
+        (await knex('user_spaces')
+          .where({ user_id: id, space_id: spaceId })
+          .first())
+      )
+    )
+      throw new ForbiddenError(
+        'you are not allowed to add members to this space'
+      )
+
+    if (!userIds?.length)
+      throw new UserInputError('must provide at least one user')
+
+    const { rows } = await knex
+      .raw(
+        `${knex('user_spaces')
+          .insert(
+            userIds.map(user_id => ({
+              user_id,
+              space_id: spaceId,
+              is_mentor: mentor,
+              is_owner: owner,
+            }))
+          )
+          .toString()} ON CONFLICT DO NOTHING RETURNING *`
+      )
+      .catch(() => {
+        throw new UserInputError('unknown space or user')
+      })
+
+    const failed = userIds.filter(
+      id => !rows.find(({ user_id }) => user_id === id)
+    )
+
+    if (failed.length)
+      throw new UserInputError(`couldn't add users ${failed.join(', ')}`)
+
+    return await query().findById(spaceId)
   }
 )
