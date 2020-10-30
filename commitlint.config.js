bad3467077858282b91dfaa0f2e67b786ba1e44f
Mathis Bullinger <mathis@bullinger.dev>
2020-10-31 00:49:34 +0100

    chore: update typescript dependencies

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'ref', 'perf', 'docs', 'test', 'build', 'ci'],
+      ['feat', 'fix', 'ref', 'perf', 'docs', 'test', 'build', 'ci', 'chore'],
     ],
   },
 }
